d640daf771bf105a1127b99c5f4a5016812cd05f
GMehandzhiyski <georgi.nikolaev.mehandjiiski@gmail.com>
2022-10-04 00:23:14 +0300

    Final ex.

---
+++ 04 - Lecture/Area/Area/Program.cs
@@ -0,0 +1,17 @@
+﻿int a = 2;
+int b = 7;
+
+int c = 7;
+int d = 8;
+
+int e = 12;
+int f = 5;
+
+int area1 = a * b;
+int area2 =  c * d;
+int area3 = e * f;
+
+
+Console.WriteLine(area1);
+Console.WriteLine(area2);
+Console.WriteLine(area3);
