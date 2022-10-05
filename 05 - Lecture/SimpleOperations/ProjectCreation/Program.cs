9e8b70039de7b0c85d0cea850a3a9884530a8152
GMehandzhiyski <georgi.nikolaev.mehandjiiski@gmail.com>
2022-10-05 16:24:08 +0300

    ProjectCreation

---
+++ 05 - Lecture/SimpleOperations/ProjectCreation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ProjectCreation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name = Console.ReadLine();
+            int Project = int.Parse(Console.ReadLine());
+
+            int Hours = Project * 3;
+
+            Console.WriteLine($" The architect {Name} will need {Hours} hours to complete {Project} project/s."); 
+
+
+
+        }
+    }
+}
