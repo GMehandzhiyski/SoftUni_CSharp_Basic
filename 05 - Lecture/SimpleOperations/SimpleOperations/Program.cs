052451e29cb05eef4ddaedda38b3e09b29b82e48
GMehandzhiyski <georgi.nikolaev.mehandjiiski@gmail.com>
2022-10-05 16:23:32 +0300

    SimpleOperations

---
+++ 05 - Lecture/SimpleOperations/SimpleOperations/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SimpleOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+
+            double inch = number * 2.54;
+
+            Console.WriteLine(inch);
+        }
+    }
+}
