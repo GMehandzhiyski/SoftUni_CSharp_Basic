d876fbcc61fe739e2fe2b7cd476f301031b2cbcd
GMehandzhiyski <georgi.nikolaev.mehandjiiski@gmail.com>
2022-10-05 16:24:38 +0300

    PetShop

---
+++ 05 - Lecture/SimpleOperations/PetShop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PetShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
