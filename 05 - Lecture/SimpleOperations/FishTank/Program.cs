8ffb328a25d78fc5837ad9a6191a26379025109f
GMehandzhiyski <georgi.nikolaev.mehandjiiski@gmail.com>
2022-10-06 14:43:05 +0300

    FishTank

---
+++ 05 - Lecture/SimpleOperations/FishTank/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data;
+
+namespace FishTank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double Length = double.Parse(Console.ReadLine());    
+            double Width = double.Parse(Console.ReadLine());
+            double Height = double.Parse(Console.ReadLine());
+            double Percent = double.Parse(Console.ReadLine());
+            double finalWater;
+            double volume;
+
+            volume = (Length * Width * Height) * 0.001;
+            finalWater = volume * (1 - (Percent * 0.01));
+
+            Console.WriteLine($"{finalWater:F3}");
+        }
+    }
+}
