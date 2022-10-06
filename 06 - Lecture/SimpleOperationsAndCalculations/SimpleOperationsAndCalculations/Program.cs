8dbf5f3f884c4742ef7b333f9287d41bd17a1fbf
GMehandzhiyski <georgi.nikolaev.mehandjiiski@gmail.com>
2022-10-06 16:33:05 +0300

    USDtoBGN

---
+++ 06 - Lecture/SimpleOperationsAndCalculations/SimpleOperationsAndCalculations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleOperationsAndCalculations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
