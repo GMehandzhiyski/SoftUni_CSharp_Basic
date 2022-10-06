5f724588f14336c85aa6dbdab7405bbe22f63af6
GMehandzhiyski <georgi.nikolaev.mehandjiiski@gmail.com>
2022-10-06 14:26:35 +0300

    YardGreening

---
+++ 05 - Lecture/SimpleOperations/YardGreening/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace YardGreening
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          double area = double.Parse(Console.ReadLine());
+          double normalPrice = 7.61;
+          double discount = 0.18;
+          double discountPrice;
+          double finalPrice;
+
+            discountPrice = (normalPrice * area) * discount;
+            finalPrice = (normalPrice * area) - discountPrice;
+
+            Console.WriteLine($"The final price is: {finalPrice:F2} lv.");
+            Console.WriteLine($"The discount is: {discountPrice:F2} lv.");
+        }
+    }
+}
