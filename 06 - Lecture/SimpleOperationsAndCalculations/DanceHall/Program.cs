9ac64f41db3809d750362f3d40ae679fba468b32
GMehandzhiyski <georgi.nikolaev.mehandjiiski@gmail.com>
2022-10-07 16:12:16 +0300

    TailoringWorkshopFinish

---
+++ 06 - Lecture/SimpleOperationsAndCalculations/DanceHall/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DanceHall
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- 06 - Lecture/SimpleOperationsAndCalculations/TailoringWorkshop/Program.cs
+++ 06 - Lecture/SimpleOperationsAndCalculations/TailoringWorkshop/Program.cs
@@ -6,7 +6,29 @@ namespace TailoringWorkshop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int tableNumber = int.Parse(Console.ReadLine());
+            double length =double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double priceUSD;
+            double priceBGN;
+            double tableclothArea;
+            double squareArea;
+
+
+
+
+            tableclothArea = (((length +  0.60) * (width + 0.60)) * tableNumber);
+            squareArea = (((length / 2) * (length / 2)) * tableNumber);
+
+
+            priceUSD = ((tableclothArea * 7) + (squareArea * 9)) ;
+            priceBGN = priceUSD * 1.85;
+
+
+
+
+            Console.WriteLine($"{priceUSD:F2} USD");
+            Console.WriteLine($"{priceBGN:F2} BGN");
         }
     }
 }
