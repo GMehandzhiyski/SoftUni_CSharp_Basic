2a7260b626edf62eb465f351a74ab9ba3155c670
GMehandzhiyski <georgi.nikolaev.mehandjiiski@gmail.com>
2022-10-08 18:58:50 +0300

    DanceHallComplate

--- 06 - Lecture/SimpleOperationsAndCalculations/DanceHall/Program.cs
+++ 06 - Lecture/SimpleOperationsAndCalculations/DanceHall/Program.cs
@@ -8,7 +8,19 @@ namespace DanceHall
         {
             double length = double.Parse(Console.ReadLine());
             double width = double.Parse(Console.ReadLine());
-            double width = double.Parse(Console.ReadLine());
+            double aWidth = double.Parse(Console.ReadLine());
+
+            double wardrobeArea = ((aWidth * 100) * (aWidth * 100));
+            double benchArea = (((length * 100) * (width * 100)) / 10);
+            double dancerArea = 7040;
+
+            double freeArea = ((length * 100) * (width * 100)) - (wardrobeArea + benchArea);
+            double dancers = freeArea / dancerArea;
+
+            Console.WriteLine(Math.Floor(dancers));
+
+
+
 
         }
     }
