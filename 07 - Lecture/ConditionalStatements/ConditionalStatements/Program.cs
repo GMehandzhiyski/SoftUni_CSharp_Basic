6f64eccb0f7449e4a9590972d65ba5cca46569d8
GMehandzhiyski <georgi.nikolaev.mehandjiiski@gmail.com>
2022-10-09 22:04:38 +0300

    Exercise1

---
+++ 07 - Lecture/ConditionalStatements/ConditionalStatements/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConditionalStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(10%3);
+        }
+    }
+}
---
+++ 07 - Lecture/ConditionalStatements/exercise1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace exercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(10 % 3);
+            Console.WriteLine(5 / 2);
+            Console.WriteLine(1 + 1 + "4" + 2 + 1);
+
+        }
+    }
+}
