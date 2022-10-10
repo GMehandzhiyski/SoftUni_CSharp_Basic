e5dddc8c2ec72c4927af79b8fbc325e627c661d3
GMehandzhiyski <georgi.nikolaev.mehandjiiski@gmail.com>
2022-10-10 22:17:32 +0300

    SumSecondsComplete

---
+++ 08 - Lecture/ConditionalStatementsExercise/SumSeconds/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SumSeconds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int firstTime = int.Parse(Console.ReadLine());
+           int secondTime = int.Parse(Console.ReadLine());
+           int thirdTime = int.Parse(Console.ReadLine());
+
+           int TotalTime = firstTime + secondTime + thirdTime;
+            int minute = TotalTime / 60;
+            int second = TotalTime % 60;
+           // Console.WriteLine($"{minute}: {second}"); only test
+
+            if (second < 10)
+            {
+                Console.WriteLine($"{minute}:0{second}");
+            }
+            else
+            {
+                Console.WriteLine($"{minute}:{second}");
+            }
+        }
+    }
+} 
