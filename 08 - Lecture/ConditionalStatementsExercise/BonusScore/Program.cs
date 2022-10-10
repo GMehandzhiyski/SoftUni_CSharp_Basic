1c01c2f30a7ef3ab05b9d2fa9696a224dbb07149
GMehandzhiyski <georgi.nikolaev.mehandjiiski@gmail.com>
2022-10-10 23:30:30 +0300

    BonusScoreComplete

---
+++ 08 - Lecture/ConditionalStatementsExercise/BonusScore/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Data;
+
+namespace BonusScore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            double bonus = 0;
+            double secBonus = 0;
+            double firstBonus = 0;
+
+            if (number <= 100)
+            {
+
+                firstBonus = number + 5;
+                if ((number % 2) == 0)
+                {
+                    secBonus = firstBonus + 1;
+                }
+                else if (number % 10 == 5)
+                {
+                    secBonus = firstBonus + 2;
+                }
+                else 
+                {
+                    secBonus = firstBonus;
+                }
+
+            }
+            else if (number > 100 && number <= 1000)
+            {
+                firstBonus = (number * 0.20) + number ;
+                if ((number % 2) == 0)
+                {
+                    secBonus = firstBonus + 1;
+                }
+                if (number % 10 == 5)
+                {
+                    secBonus = firstBonus + 2;
+                }
+            }
+            else
+                firstBonus = (number * 0.10) + number;
+                if ((number % 2) == 0)
+                 {
+                secBonus = firstBonus + 1;
+                }
+                if (number % 10 == 5)
+                {
+                secBonus = firstBonus + 2;
+                }
+
+            
+            bonus = secBonus - number;
+            
+            Console.WriteLine($"{bonus}");
+            Console.WriteLine($"{secBonus}");
+        }
+    }
+}
