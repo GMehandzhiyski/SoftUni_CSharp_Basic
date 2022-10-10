e59058502ea529c53ded29d7abfd0e4b89c0c2f9
GMehandzhiyski <georgi.nikolaev.mehandjiiski@gmail.com>
2022-10-11 00:11:26 +0300

    MetricConverterComplete

---
+++ 08 - Lecture/ConditionalStatementsExercise/MetricConverter/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace MetricConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double inputValue = double.Parse(Console.ReadLine());
+            string Input = Console.ReadLine();
+            string Output = Console.ReadLine();
+            double outputValue;
+
+
+            if (Input == "mm")
+            {
+                if (Output == "m")
+                {
+                    outputValue = inputValue / 1000;
+                }
+                else // cm
+                {
+                    outputValue = inputValue / 10;
+                }
+
+            }
+            else if (Input == "m")
+            {
+                if(Output == "mm")
+                {
+                    outputValue = inputValue * 1000;
+                }
+                else //cm
+                {
+                    outputValue = inputValue * 100;
+                }
+            }
+            else  // cm
+            {
+                if (Output == "m")
+                {
+                    outputValue = inputValue / 100;
+                }
+                else // mm
+                {
+                    outputValue = inputValue * 10;
+                }
+            }
+
+            Console.WriteLine($"{outputValue:F3}");
+
+
+        }
+    }
+}
