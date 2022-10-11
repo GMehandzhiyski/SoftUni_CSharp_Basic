7c122f26bf0fd3b9e7186397510f1994d5de4a1c
GMehandzhiyski <georgi.nikolaev.mehandjiiski@gmail.com>
2022-10-11 23:56:33 +0300

    GodzillaVsKong

---
+++ 08 - Lecture/ConditionalStatementsExercise/GodzillaVsKong/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GodzillaVsKong
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
