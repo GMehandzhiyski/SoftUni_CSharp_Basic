998b399a27ccbedc28e3371c6322631669192903
GMehandzhiyski <georgi.nikolaev.mehandjiiski@gmail.com>
2022-10-11 23:26:46 +0300

    Time+15Minutes

---
+++ 08 - Lecture/ConditionalStatementsExercise/Time+15Minutes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Time_15Minutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hoursIn = int.Parse(Console.ReadLine());
+            int minutesIn = int.Parse(Console.ReadLine());
+            int minutesMeddleValue = 0;
+            int minutesMeddleValue1 = 0;
+            int hoursOut = 0;
+            int minutesOut = 0;
+
+
+            minutesMeddleValue = minutesIn + 15;
+            if (minutesMeddleValue > 59)
+            {
+                minutesMeddleValue1 = minutesMeddleValue - 15;
+                if (hoursIn == 24 )
+                {
+                    hoursOut = 0;
+                }
+                else
+                    hoursOut = hoursIn + 1;
+
+            }
+            else
+            { 
+            
+            }
+
+
+        }
+    }
+}
