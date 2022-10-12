7352fd6dc36de0e80f969a7b4bae827cb2a14d7e
GMehandzhiyski <georgi.nikolaev.mehandjiiski@gmail.com>
2022-10-12 11:34:47 +0300

    WordSwimmingRecordComplete

--- 08 - Lecture/ConditionalStatementsExercise/WordSwimmingRecord/Program.cs
+++ 08 - Lecture/ConditionalStatementsExercise/WordSwimmingRecord/Program.cs
@@ -6,6 +6,22 @@ namespace WordSwimmingRecord
     {
         static void Main(string[] args)
         {
+            double recordTime = double.Parse(Console.ReadLine());
+            double range = double.Parse(Console.ReadLine());
+            double timePer1km = double.Parse(Console.ReadLine());
+
+            double delayTime = (Math.Floor(range / 15) * 12.5);//time in second
+            double timeIvan = (range * timePer1km) + delayTime;//total time Ivan
+
+            if (recordTime > timeIvan)
+            {
+                Console.WriteLine($" Yes, he succeeded! The new world record is {timeIvan:F2} seconds.");
+            }
+            else
+            {
+                Console.WriteLine($"No, he failed! He was {timeIvan - recordTime:F2} seconds slower");
+            }
+               
             
         }
     }
