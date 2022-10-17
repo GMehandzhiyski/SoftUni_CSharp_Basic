774378cfbc96822774ea6c7222838587e4b0801b
GMehandzhiyski <georgi.nikolaev.mehandjiiski@gmail.com>
2022-10-17 22:34:38 +0300

    InvalidNumberComplete

--- 09 - Lecture/ConditionalStatementsAdvanced/InvalidNumber/Program.cs
+++ 09 - Lecture/ConditionalStatementsAdvanced/InvalidNumber/Program.cs
@@ -7,7 +7,15 @@ namespace InvalidNumber
         static void Main(string[] args)
         {
             int number = int.Parse(Console.ReadLine());
-            if
+
+            if ((number >= 100 && number <= 200) || number == 0)
+            {
+                Console.WriteLine("");
+            }
+            else
+            {
+                Console.WriteLine("invalid");
+            }
 
         }
     }
