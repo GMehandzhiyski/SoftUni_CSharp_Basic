1cafd1435f0ef7c244a6c97d6e545eb90a6adb73
GMehandzhiyski <georgi.nikolaev.mehandjiiski@gmail.com>
2022-10-19 23:16:59 +0300

    SummerOutfitColmplete

--- 10 - Lecture/Conditional Statements Advanced - Exercise/SummerOutfit/Program.cs
+++ 10 - Lecture/Conditional Statements Advanced - Exercise/SummerOutfit/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.Unicode;
 
 namespace SummerOutfit
 {
@@ -8,24 +9,69 @@ namespace SummerOutfit
         {
             int degrees = int.Parse(Console.ReadLine());
             string timeOfday = Console.ReadLine();
+            string outfit = "";
+            string shoes = "";
 
 
-            if (10 <= degrees && degrees <= 18 )
+            if (10 <= degrees && degrees <= 18)
             {
                 if (timeOfday == "Morning")
                 {
+                    outfit = "Sweatshirt";
+                    shoes = "Sneakers";
 
                 }
                 else if (timeOfday == "Afternoon")
-                { 
-                
+                {
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
                 }
                 else if (timeOfday == "Evening")
                 {
-
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
                 }
 
             }
+            else if (18 < degrees && degrees <= 24)
+            {
+                if (timeOfday == "Morning")
+                {
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                }
+                else if (timeOfday == "Afternoon")
+                {
+                    outfit = "T-Shirt";
+                    shoes = "Sandals";
+                }
+                else if (timeOfday == "Evening")
+                {
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                }
+
+
+            }
+            else if (degrees >= 25)
+            {
+                if (timeOfday == "Morning")
+                {
+                    outfit = "T-Shirt";
+                    shoes = "Sandals";
+                }
+                else if (timeOfday == "Afternoon")
+                {
+                    outfit = "Swim Suit";
+                    shoes = "Barefoot";
+                }
+                else if (timeOfday == "Evening")
+                {
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                }
+            }
+            Console.WriteLine($"It's {degrees} degrees, get your {outfit} and {shoes}.");
         }
     }
 }
