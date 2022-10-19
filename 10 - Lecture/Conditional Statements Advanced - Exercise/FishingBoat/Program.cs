fac6584f9c98e57bd39e65d7a1227edcdf3b5dad
GMehandzhiyski <georgi.nikolaev.mehandjiiski@gmail.com>
2022-10-19 23:54:51 +0300

    Update

---
+++ 10 - Lecture/Conditional Statements Advanced - Exercise/FishingBoat/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FishingBoat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int buget = int.Parse(Console.ReadLine());
+            string Season = Console.ReadLine();
+            int numberFish = int.Parse(Console.ReadLine()); 
+
+
+        }
+    }
+}
