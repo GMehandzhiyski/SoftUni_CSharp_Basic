1e99b5c249c6d1649aeb12926ac7789128bc5a60
GMehandzhiyski <georgi.nikolaev.mehandjiiski@gmail.com>
2022-10-19 22:59:39 +0300

    up

---
+++ 10 - Lecture/Conditional Statements Advanced - Exercise/SummerOutfit/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SummerOutfit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int degrees = int.Parse(Console.ReadLine());
+            string timeOfday = Console.ReadLine();
+
+
+            if (10 <= degrees && degrees <= 18 )
+            {
+                if (timeOfday == "Morning")
+                {
+
+                }
+                else if (timeOfday == "Afternoon")
+                { 
+                
+                }
+                else if (timeOfday == "Evening")
+                {
+
+                }
+
+            }
+        }
+    }
+}
