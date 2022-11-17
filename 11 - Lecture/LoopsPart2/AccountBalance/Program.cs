df873b86bed60f7a2a6316a3b352e2d5797f3296
GMehandzhiyski <georgi.nikolaev.mehandjiiski@gmail.com>
2022-11-17 23:34:12 +0200

    AccountBalanceInputDone

---
+++ 11 - Lecture/LoopsPart2/AccountBalance/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace AccountBalance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int transactionNumber = int.Parse(Console.ReadLine());
+            double moneySum;
+            int whileCout = 0;
+            double totalSum = 0;
+
+            if (transactionNumber > 0)
+            {
+                while (whileCout < transactionNumber)
+                {
+                    moneySum = int.Parse(Console.ReadLine());
+                    /*
+                    Console.WriteLine($"Increase:{moneySum}");
+                    */
+                    totalSum = totalSum + moneySum;
+                    
+                    whileCout = whileCout + 1;
+                }
+
+                if (whileCout >= transactionNumber)
+                {
+                    Console.WriteLine($"Total:{totalSum}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid operation!");
+            }
+
+        }
+    }
+}
