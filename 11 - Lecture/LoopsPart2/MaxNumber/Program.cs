315cf06e8e18cc216cba84404522fa7f8436ad3c
GMehandzhiyski <georgi.nikolaev.mehandjiiski@gmail.com>
2022-11-21 23:04:29 +0200

    MaxNumberCreate

---
+++ 11 - Lecture/LoopsPart2/MaxNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MaxNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = Int32.MinValue;
+            int number = int.Parse(Console.ReadLine());
+            int cycles = 0;
+
+
+            while ( number >= cycles)
+            {
+
+                Console.WriteLine("cycle");
+                cycles++;
+            }
+        }
+    }
+}
