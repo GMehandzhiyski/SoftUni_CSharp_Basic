570f401c4ef28e06d40bffcf21bfd74b029f4161
GMehandzhiyski <georgi.nikolaev.mehandjiiski@gmail.com>
2022-11-22 22:41:08 +0200

    GraduationDone

--- 11 - Lecture/LoopsPart2/Graduation/Program.cs
+++ 11 - Lecture/LoopsPart2/Graduation/Program.cs
@@ -6,7 +6,36 @@ namespace Graduation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           string name = Console.ReadLine();
+           int cycleWhile = 12;
+           int cycleCount = 0;
+           double grade = 0.0;
+           double finalGrade = 0;
+
+
+
+            while (cycleWhile > cycleCount)
+            { 
+               grade = double.Parse(Console.ReadLine());
+
+                finalGrade = (finalGrade + grade);
+
+                cycleCount++;
+            }
+
+            if (cycleWhile == cycleCount)
+            {
+                if (finalGrade >= 4.00)
+                {
+                    Console.WriteLine($"{name} graduated. Average grade: {finalGrade/12:f2}");
+                }
+              /*  else
+                { 
+                    Console.WriteLine($"{name} not graduated. Averade grade: {finalGrade:f2}");
+                }
+               */    
+
+            }
         }
     }
 }
