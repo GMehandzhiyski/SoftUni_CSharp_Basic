8954c456d66ac81b4935d3836a39fa303b222d6c
GMehandzhiyski <georgi.nikolaev.mehandjiiski@gmail.com>
2022-11-23 23:50:17 +0200

    OldBooksDone

--- 12 - Lecture/Loops - Part 2 - Exercise/OldBooks/Program.cs
+++ 12 - Lecture/Loops - Part 2 - Exercise/OldBooks/Program.cs
@@ -7,28 +7,7 @@ namespace OldBooks
     {
         static void Main(string[] args)
         {
-            string theBook = Console.ReadLine();
-            int capacityLib = int.Parse(Console.ReadLine());
-            string othersBook ="" ;
-            int cycleCounter = 0;
            
-            while (capacityLib > cycleCounter && othersBook != theBook)
-            {
-                othersBook = Console.ReadLine();
-                
-                cycleCounter++;
-            }
-
-            if (capacityLib == cycleCounter)
-            {
-                Console.WriteLine("The book you search is not here!");
-                Console.WriteLine($"You checked {cycleCounter} books.");
-            }
-       
-            if ( othersBook == theBook)
-            {
-                Console.WriteLine($"You checked {cycleCounter - 1} books and found it.");  
-            }
         
         }
     }
---
+++ 12 - Lecture/Loops - Part 2 - Exercise/OldBooks2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace OldBooks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string theBook = Console.ReadLine();
+            int capacityLib = Math.Abs(int.Parse(Console.ReadLine()));
+            string othersBook = "";
+            int cycleCounter = 0;
+
+            while (capacityLib > cycleCounter && othersBook != theBook)
+            {
+                othersBook = Console.ReadLine();
+                
+                if (othersBook == theBook)
+                {
+                    Console.WriteLine($"You checked {cycleCounter} books and found it.");
+                    break;
+                }
+
+                cycleCounter++;
+            }
+
+            if (capacityLib == cycleCounter)
+            {
+                Console.WriteLine("The book you search is not here!");
+                Console.WriteLine($"You checked {cycleCounter} books.");
+            }
+
+            
+        }
+    }
+}
