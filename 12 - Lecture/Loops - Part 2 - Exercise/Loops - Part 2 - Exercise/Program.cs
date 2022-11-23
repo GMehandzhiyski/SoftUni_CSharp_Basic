dbcdfd3413a1b47e5e64d50e4d89656401598b3f
GMehandzhiyski <georgi.nikolaev.mehandjiiski@gmail.com>
2022-11-23 23:34:49 +0200

    OldBooksCreate

---
+++ 12 - Lecture/Loops - Part 2 - Exercise/Loops - Part 2 - Exercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Loops___Part_2___Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
