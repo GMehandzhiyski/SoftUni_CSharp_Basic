3e5f7e2331be9cd86d7241b58a61d50b4ca60ba9
GMehandzhiyski <georgi.nikolaev.mehandjiiski@gmail.com>
2022-11-25 16:20:51 +0200

    ExamPreparation95%done

--- 12 - Lecture/Loops - Part 2 - Exercise/ExamPreparation/Program.cs
+++ 12 - Lecture/Loops - Part 2 - Exercise/ExamPreparation/Program.cs
@@ -6,7 +6,42 @@ namespace ExamPreparation
     {
         static void Main(string[] args)
         {
+            int badGrade = int.Parse(Console.ReadLine());
+            string nameEx = String.Empty;
+            int grade = 0;
+            int badGradeCount = 0;
+            int whileCount = 0;
+            double averageGrade = 0;
+
+
+            while((nameEx = Console.ReadLine()) != "Enough") 
+            {
+                grade = int.Parse(Console.ReadLine());
+                whileCount++;
+                averageGrade = (grade + averageGrade)/whileCount;
+
+                if (grade <= 4.00)
+                {
+                    badGradeCount++;
+                }
+                if (badGrade == badGradeCount)
+                {
+                    Console.WriteLine($"You need a break, {badGrade} poor grades.");
+                    break;
+                }
+            }
+
+            if (nameEx == "Enough")
+            {
+                Console.WriteLine($"Average score: { averageGrade:f2}");
+                Console.WriteLine($"Number of problems: {whileCount}");
+                Console.WriteLine($"Last problem: {nameEx}");
+
+            }
+
+
             
+
         }
     }
 }
