e56ac84da059136ec7f07d3558dd1aa20dd89419
GMehandzhiyski <georgi.nikolaev.mehandjiiski@gmail.com>
2022-12-03 23:22:10 +0200

    CoinsCreate

---
+++ 12 - Lecture/Loops - Part 2 - Exercise/Coins/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Coins
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double inputSum = double.Parse(Console.ReadLine());
+            double outputSum = 0;
+            int twoLvCoins = 0;
+
+            if (inputSum > 2)
+            {
+                inputSum = inputSum - 2;
+                twoLvCoins++;
+            }
+            if (inputSum > 1)
+            {
+                inputSum = inputSum - 1;
+
+            }
+
+
+        }
+    }
+}
