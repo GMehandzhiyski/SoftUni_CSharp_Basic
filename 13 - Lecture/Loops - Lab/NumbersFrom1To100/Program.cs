6828290913dc660ccece5de73e70755e6062ab78
GMehandzhiyski <georgi.nikolaev.mehandjiiski@gmail.com>
2022-12-06 18:44:22 +0200

    NumbersFrom1To100100%Done

---
+++ 13 - Lecture/Loops - Lab/NumbersFrom1To100/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NumbersFrom1To100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { int i = 0;
+            for (i = 1; i <= 100; i++) 
+            {
+            Console.WriteLine(i);
+            }
+        }
+    }
+}
