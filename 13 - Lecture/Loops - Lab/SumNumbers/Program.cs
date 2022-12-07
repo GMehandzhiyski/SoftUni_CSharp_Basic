7dc8f478357c3f4bc619dca92e5a62f7b63582f3
GMehandzhiyski <georgi.nikolaev.mehandjiiski@gmail.com>
2022-12-07 16:20:22 +0200

    SumNumbers100%Done

---
+++ 13 - Lecture/Loops - Lab/SumNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SumNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantityNumbers = int.Parse(Console.ReadLine());
+            int number = 0;
+            int numberResult = 0;
+            int forCounter = 0;
+
+
+            for (int i = 0; i < quantityNumbers; i++)
+            {
+                number = int.Parse(Console.ReadLine());
+                numberResult = numberResult + number;
+                forCounter++;
+            }
+
+            if (forCounter == quantityNumbers)
+            {
+                Console.WriteLine(numberResult);
+            }
+        }
+    }
+}
