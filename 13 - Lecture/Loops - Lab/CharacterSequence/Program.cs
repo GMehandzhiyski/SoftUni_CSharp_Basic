a97e6bf9630cf694a46f50f4b128cbe28b821928
GMehandzhiyski <georgi.nikolaev.mehandjiiski@gmail.com>
2022-12-11 23:22:51 +0200

    CharacterSequence 100% Done

--- 13 - Lecture/Loops - Lab/CharacterSequence/Program.cs
+++ 13 - Lecture/Loops - Lab/CharacterSequence/Program.cs
@@ -6,7 +6,13 @@ namespace CharacterSequence
     {
         static void Main(string[] args)
         {
-            
+            string input = Console.ReadLine();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char charAt = input[i];
+                Console.WriteLine(charAt);  
+            }
 
             
         }
