39da8138a677320222452771cadaecbac66533e0
GMehandzhiyski <georgi.nikolaev.mehandjiiski@gmail.com>
2022-12-12 22:58:34 +0200

    Clever Lily Create

---
+++ 13 - Lecture/Loops - Lab/Clever Lily/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Clever_Lily
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
