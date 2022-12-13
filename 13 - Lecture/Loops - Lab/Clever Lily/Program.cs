2d0a81f92832b1ba48de5bfdf0b5d2c44b044035
GMehandzhiyski <georgi.nikolaev.mehandjiiski@gmail.com>
2022-12-13 23:12:43 +0200

    Clever Lily 100% Done

--- 13 - Lecture/Loops - Lab/Clever Lily/Program.cs
+++ 13 - Lecture/Loops - Lab/Clever Lily/Program.cs
@@ -6,7 +6,44 @@ namespace Clever_Lily
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double ageLily = double.Parse(Console.ReadLine());
+            double washingMachine = double.Parse(Console.ReadLine());
+            double toyPricePerPieces = double.Parse(Console.ReadLine());
+            double toyNumber = 0;
+            double money = 0;
+            double toyPrice = 0;
+            double savedMoney = 0;
+            int moneyCounter = 0;
+
+            for (int i = 1; i <= ageLily; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    money += (10);
+                    savedMoney += money; 
+                    moneyCounter++;
+
+                }
+                else
+                {
+                    toyNumber++;
+                }
+
+            }
+
+            toyPrice = toyNumber * toyPricePerPieces;
+            savedMoney += (toyPrice - moneyCounter);
+
+            if (savedMoney >= washingMachine)
+            {
+                Console.WriteLine($"Yes! {(savedMoney - washingMachine):F2}");
+            }
+            if(savedMoney < washingMachine)
+            {
+                Console.WriteLine($"No! {Math.Abs(washingMachine - savedMoney):F2}");
+            }
+
+
         }
     }
 }
