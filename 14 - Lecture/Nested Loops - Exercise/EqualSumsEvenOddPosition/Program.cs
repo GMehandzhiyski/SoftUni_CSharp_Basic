019ac9dc9e8ec61160818810d73e9150bc9c3690
GMehandzhiyski <georgi.nikolaev.mehandjiiski@gmail.com>
2022-12-13 23:47:10 +0200

    EqualSumsEvenOddPosition Create

---
+++ 14 - Lecture/Nested Loops - Exercise/EqualSumsEvenOddPosition/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EqualSumsEvenOddPosition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
