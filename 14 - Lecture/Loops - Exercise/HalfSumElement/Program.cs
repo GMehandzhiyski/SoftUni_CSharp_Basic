f66f91f9289494accaf33fe618defa18e5ac1160
GMehandzhiyski <georgi.nikolaev.mehandjiiski@gmail.com>
2022-12-15 22:40:55 +0200

    HalfSumElement Create

---
+++ 14 - Lecture/Loops - Exercise/HalfSumElement/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HalfSumElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
