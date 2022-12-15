4334cf40ee4cb4dcf1da7e1167aebe10cb2d582b
GMehandzhiyski <georgi.nikolaev.mehandjiiski@gmail.com>
2022-12-15 23:06:45 +0200

    HalfSumElement 100% Done

--- 14 - Lecture/Loops - Exercise/HalfSumElement/Program.cs
+++ 14 - Lecture/Loops - Exercise/HalfSumElement/Program.cs
@@ -6,7 +6,37 @@ namespace HalfSumElement
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            int num = 0;
+            int sum = 0; 
+            int max = int.MinValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                num = int.Parse(Console.ReadLine());
+                sum += num;
+
+                if (num > max)
+                {
+                    max = num;
+                }
+
+            }
+            int sumWitoutMaxNumber = sum - max;
+            if (max == sumWitoutMaxNumber)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine("Sum = " + max);
+            }
+            else
+            {
+                int diff = Math.Abs(max - sumWitoutMaxNumber);
+                Console.WriteLine("No");
+                Console.WriteLine("Diff = " + diff);
+            
+            
+            }
+
         }
     }
 }
