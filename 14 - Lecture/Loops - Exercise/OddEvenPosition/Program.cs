69deb4720615cb72d257a4d3591c969f29239fb7
GMehandzhiyski <georgi.nikolaev.mehandjiiski@gmail.com>
2022-12-16 16:08:18 +0200

    OddEvenPosition 100% Done

--- 14 - Lecture/Loops - Exercise/OddEvenPosition/Program.cs
+++ 14 - Lecture/Loops - Exercise/OddEvenPosition/Program.cs
@@ -6,6 +6,8 @@ namespace OddEvenPosition
     {
         static void Main(string[] args)
         {
+
+            
             int n = int.Parse(Console.ReadLine());
             double sumEven = 0;
             double sumOdd = 0;
@@ -20,40 +22,59 @@ namespace OddEvenPosition
             {
                 double number = double.Parse(Console.ReadLine());
 
-                if (number % 2 == 0)//Even
+                if (i % 2 == 0)//Even
                 {
                    sumEven += number;
-                    if (number > maxEven) 
+                    if (number > maxEven)//MAX
                     {
                         maxEven = number;
                     }
-                    if (number < minEven)
+                    if (number < minEven)//MIN
                     { 
                         minEven = number;
                     }
 
                     //Console.WriteLine(i);
                 }
-                else 
+                else //Odd
                 {
                     sumOdd += number;
-                    if (number > maxOdd)
+                    if (number > maxOdd)//MAX
                     {
                         maxOdd = number;
                     }
-                    if (number < minOdd)
+                    if (number < minOdd)//MIN
                     {
                         minOdd = number;
                     }
                 }
             }
-            Console.WriteLine($"OddSum={sumOdd:f2}");
-            Console.WriteLine($"OddMin={minOdd:f2}");
-            Console.WriteLine($"OddMax={maxOdd:f2}");
 
-            Console.WriteLine($"EvenSum={sumEven:f2}");
-            Console.WriteLine($"EvenMin={minEven:f2}");
-            Console.WriteLine($"EvenMax={maxEven:f2}");
+            Console.WriteLine($"OddSum={sumOdd:f2},");
+            if (n == 0 )
+            {
+                Console.WriteLine($"OddMin=No,");
+                Console.WriteLine($"OddMax=No,");
+            }
+            else 
+            {
+                Console.WriteLine($"OddMin={minOdd:f2},");
+                Console.WriteLine($"OddMax={maxOdd:f2},");
+            }
+
+            Console.WriteLine($"EvenSum={sumEven:f2},");
+            if (n == 0 || n == 1)
+            {
+                Console.WriteLine($"EvenMin=No,");
+                Console.WriteLine($"EvenMax=No");
+            }
+            else 
+            {
+                Console.WriteLine($"EvenMin={minEven:f2},");
+                Console.WriteLine($"EvenMax={maxEven:f2}");
+            }
+            
         }
+            
     }
 }
