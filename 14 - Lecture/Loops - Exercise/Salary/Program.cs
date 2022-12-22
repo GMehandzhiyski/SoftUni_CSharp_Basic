2ef472ec3b2353446fff953ab97fda1e780af07f
GMehandzhiyski <georgi.nikolaev.mehandjiiski@gmail.com>
2022-12-22 16:29:43 +0200

    Salary 100% Done

--- 14 - Lecture/Loops - Exercise/Salary/Program.cs
+++ 14 - Lecture/Loops - Exercise/Salary/Program.cs
@@ -6,7 +6,51 @@ namespace Salary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           double tabs = double.Parse(Console.ReadLine());
+           double salary = double.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= tabs; i++)
+            {
+                string webSite = Console.ReadLine();
+
+                if (webSite == "Facebook")
+                {
+                    salary -= 150;
+
+                    if (salary <= 0)
+                    {
+                        Console.WriteLine("You have lost your salary.");
+                        break;
+                    }
+                }
+                else if (webSite == "Instagram")
+                {
+                    salary -= 100;
+
+                    if (salary <= 0)
+                    {
+                        Console.WriteLine("You have lost your salary.");
+                        break;
+                    }
+                }
+                else if (webSite == "Reddit")
+                {
+
+                    salary -= 50;
+
+                    if (salary <= 0)
+                    {
+                        Console.WriteLine("You have lost your salary.");
+                        break;
+                    }
+                }
+
+                if (i == tabs)
+                {
+                    Console.WriteLine(salary);
+                }
+
+            }
         }
     }
 }
