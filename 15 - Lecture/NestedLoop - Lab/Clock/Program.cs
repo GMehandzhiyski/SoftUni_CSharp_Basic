5e75f716322734a8907b3b9b9cfec4403caa514f
GMehandzhiyski <georgi.nikolaev.mehandjiiski@gmail.com>
2022-12-23 09:17:47 +0200

    Clock Crated

---
+++ 15 - Lecture/NestedLoop - Lab/Clock/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Clock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 24; i++)
+            {
+                for (int ii = 0; ii < 60; ii++)
+                {
+                    Console.WriteLine($"{i}:{ii}");
+                }
+            }
+        }
+    }
+}
---
+++ 15 - Lecture/NestedLoop - Lab/NestedLoop - Lab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NestedLoop___Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
