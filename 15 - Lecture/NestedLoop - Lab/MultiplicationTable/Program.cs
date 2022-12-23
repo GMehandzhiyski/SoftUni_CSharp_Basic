b8aa6f9f3220c5c60b6a5477d38f2afe45f4651d
GMehandzhiyski <georgi.nikolaev.mehandjiiski@gmail.com>
2022-12-23 09:26:39 +0200

    MultiplicationTable 100% Done

---
+++ 15 - Lecture/NestedLoop - Lab/MultiplicationTable/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+namespace MultiplicationTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int ii = 1; ii <= 10; ii++)
+                {
+                    Console.WriteLine($"{i} * {ii} = {i*ii}");
+                }
+            }
+        }
+    }
+}
