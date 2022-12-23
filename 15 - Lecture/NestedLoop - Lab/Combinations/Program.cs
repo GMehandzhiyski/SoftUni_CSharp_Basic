6d2e8baab04f324d1308c3df0a8b4a1a8f200f31
GMehandzhiyski <georgi.nikolaev.mehandjiiski@gmail.com>
2022-12-23 09:56:10 +0200

    Combinations 88% Done

---
+++ 15 - Lecture/NestedLoop - Lab/Combinations/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics.Tracing;
+using System.Runtime.CompilerServices;
+
+namespace Combinations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            int n = int.Parse(Console.ReadLine());
+            int ij = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int ii = 0; ii <= n; ii++)
+                {
+                    for (int iii = 0; iii <= n; iii++)
+                    {
+                        if (i + ii + iii == n)
+                        {
+                           
+                            counter++;
+                        }
+                      
+
+                    }
+                }
+                ij = i;
+            }
+            if (ij == n)
+            {
+                Console.WriteLine(counter);
+            }
+            
+
+
+        }
+    }
+}
