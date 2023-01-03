7f5017ff496e7684d578768fd03b8548e3cf3880
GMehandzhiyski <georgi.nikolaev.mehandjiiski@gmail.com>
2023-01-03 22:29:25 +0200

    Traveling is done on 50%

---
+++ 15 - Lecture/NestedLoop - Lab/Traveling/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Traveling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstWhile = 0;
+            int bugget = 0;
+            string destination = string.Empty;
+            string country = string.Empty;
+            int sumTotal = 0;
+/*
+            while (firstWhile == 0 )
+            {
+                destination = Console.ReadLine();
+                bugget = int.Parse(Console.ReadLine());
+                firstWhile++;
+                country = destination;
+
+            }
+  */          
+           
+            while ((destination = Console.ReadLine()) != "End" )
+            {
+                while (firstWhile == 0)
+                {
+                    //destination = Console.ReadLine();
+                    bugget = int.Parse(Console.ReadLine());
+                    firstWhile++;
+                    country = destination;
+
+                }
+                int sum = int.Parse(destination);
+                
+                sumTotal += sum;
+                while (bugget <= sumTotal)
+                {
+                        Console.WriteLine($"Going to {country}!");
+                        destination = String.Empty;
+                        sumTotal = 0;
+                        firstWhile = 0;
+
+                }
+               
+
+            }
+            if (destination == "End")
+            {
+                return;
+            }
+        }
+    }
+}
+
